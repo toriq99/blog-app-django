e80e6fc4eddfd983d7427a7a7cb334b943522510
Thariq <thariq879@gmail.com>
2023-01-20 18:41:10 +0700

    dynamic view

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('home/', views.home, name="home-page"),
     path('posts/', views.posts, name='posts-page'),
     path('posts/<slug:slug>', views.posts_details, name='posts-detail-page') # /posts/my-first-page
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,85 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import date
+
+post_data = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Ariq",
+        "date": date(2022, 1, 10),
+        "title": "Mountain Hiking",
+        "excerpt": "This is my first trip to this mountain, the view is so good!, i hope you can enjoy the views too!",
+        "content": """       
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+        """
+    },
+    { 
+        "slug": "learn-django",
+        "image": "django.png",
+        "author": "Ariq",
+        "date": date(2022, 2, 13),
+        "title": "Learn Django",
+        "excerpt": "My first step learning django!",
+        "content": """       
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+        """
+    },
+    {
+        "slug": "at-the-zoo",
+        "image": "zoo.jpg",
+        "author": "Ariq",
+        "date": date(2022, 1, 14),
+        "title": "Holiday at Zoo",
+        "excerpt": "Zoo is fun, i hope you can enjoy the views too!",
+        "content": """       
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+        """
+    },
+    {
+        "slug": "riding-motocycle",
+        "image": "riding.jpg",
+        "author": "Ariq",
+        "date": date(2022, 3, 24),
+        "title": "Riding Motocycle",
+        "excerpt": "One of my hobbies, its very fun!",
+        "content": """       
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
+        """
+    }
+]
+
+def get_date(post_data):
+    return post_data['date']
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html')
+    sorted_post = sorted(post_data, key=get_date)
+    latest_post = sorted_post[-3:]
+    return render(request, 'blog/index.html',{
+        "post": latest_post,
+    })
 
 def posts(request):
-    return HttpResponse("post url success")
+    return render(request, 'blog/all-posts.html')
 
-def posts_details(request):
-    return HttpResponse("spesific post url success")
-
-def home(request):
-    return render(request, 'blog/homepage-1.html')
+def posts_details(request, slug):
+    return render(request, 'blog/posts-details.html')
