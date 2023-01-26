230136b0b7d5cdd3ba0f9822e0c54dcde9220429
Thariq <thariq879@gmail.com>
2023-01-26 10:17:23 +0700

    convert id in url to slug & using aggregate function

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,10 +11,14 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     authors = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True)
 
     def get_absolute_url(self):
-        return reverse("book-detail", args=[self.id])
+        return reverse("book-detail", args=[self.slug])
     
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)                                                                         
+        super().save(*args, **kwargs)
 
     # make object in terminal more readable
     def __str__(self):
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from .models import Book
+from django.db.models import Avg
 
 # Create your views here.
 
 def index(request):
-    book_model = Book.objects.all()
+    book_model = Book.objects.all().order_by("-rating")
+    num_books = book_model.count()
+    avg_rating = book_model.aggregate(Avg('rating')) # use rating__avg in templates
+
     return render(request, "book_outlet/index.html", {
         'books': book_model,
+        'books_total': num_books,
+        'average_rating': avg_rating
     })
 
-def book_detail(request, id):
-    selected_book = Book.objects.get(pk = id)
+def book_detail(request, slug):
+    # bisa menggunakan get_object_or_404
+    """
+    contoh :
+    book = get_object_or_404(Book, slug = slug)
+
+    """
+    selected_book = Book.objects.get(slug = slug)
     return render(request, "book_outlet/book_detail.html", {
         'book': selected_book
     })
