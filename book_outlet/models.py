5624f1c38815a61f25bcf410cb6aae50c2c73ed2
Thariq <thariq879@gmail.com>
2023-01-27 17:50:21 +0700

    adding comment

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,6 +5,7 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+# Tabel Country
 class Country(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=3)
@@ -15,7 +16,7 @@ class Country(models.Model):
     class Meta:
         verbose_name_plural = "Countries"
 
-
+# Tabel Address
 class Address(models.Model):
     street = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=5)
@@ -27,11 +28,11 @@ class Address(models.Model):
     class Meta:
         verbose_name_plural = "Address Entries"
 
-
+# Tabel Author
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True) # Relation One-to-One
 
     def fullname(self):
         return f"{self.first_name} {self.last_name}"
@@ -39,15 +40,15 @@ class Author(models.Model):
     def __str__(self):
         return self.fullname()
 
-
+# Tabel Book
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    authors = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    authors = models.ForeignKey(Author, on_delete=models.CASCADE, null=True) # Relation One-To-Many (foreign key in many)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
-    published_country = models.ManyToManyField(Country)
+    published_country = models.ManyToManyField(Country) # Realtion Many-to-Many
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
