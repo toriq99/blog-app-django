6c543f81c429974e8ddc513db5fa59ad04a8efe4
Thariq <thariq879@gmail.com>
2023-01-27 09:52:43 +0700

    adding author foreign key in Book class

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,11 +5,15 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    authors = models.CharField(null=True, max_length=100)
+    authors = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
