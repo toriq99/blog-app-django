fb093f8aadd288e20b876b8394edcfeeee3e5550
Thariq <thariq879@gmail.com>
2023-02-01 09:00:52 +0700

    change blog app data with database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100, default='FIRST NAME')
+    last_name = models.CharField(max_length=100, default='LAST NAME')
+    email = models.EmailField(default='SOME STRING')
+
+    def __str__(self):
+        return f"{self.first_name}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, null=True)
+    excerpt = models.CharField(max_length=50, null=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    image_name = models.CharField(max_length=50, null=True)
+    date = models.DateField(auto_now=True, null=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)], null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.title}, {self.author}"
--- blog/views.py
+++ blog/views.py
@@ -1,90 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
 
-post_data = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Ariq",
-        "date": date(2022, 1, 10),
-        "title": "Mountain Hiking",
-        "excerpt": "This is my first trip to this mountain, the view is so good!, i hope you can enjoy the views too!",
-        "content": """       
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-        """
-    },
-    { 
-        "slug": "learn-django",
-        "image": "django.png",
-        "author": "Ariq",
-        "date": date(2022, 2, 13),
-        "title": "Learn Django",
-        "excerpt": "My first step learning django!",
-        "content": """       
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-        """
-    },
-    {
-        "slug": "at-the-zoo",
-        "image": "zoo.jpg",
-        "author": "Ariq",
-        "date": date(2022, 1, 14),
-        "title": "Holiday at Zoo",
-        "excerpt": "Zoo is fun, i hope you can enjoy the views too!",
-        "content": """       
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-        """
-    },
-    {
-        "slug": "riding-motocycle",
-        "image": "riding.jpg",
-        "author": "Ariq",
-        "date": date(2022, 3, 24),
-        "title": "Riding Motocycle",
-        "excerpt": "One of my hobbies, its very fun!",
-        "content": """       
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam in, totam error nesciunt consequuntur ad sit necessitatibus, fugit perferendis enim, sapiente vero rerum odio qui. Nostrum ullam accusantium quidem.
-        """
-    }
-]
-
-def get_date(post_data):
-    return post_data['date']
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    sorted_post = sorted(post_data, key=get_date)
-    latest_post = sorted_post[-3:]
+    latest_post = Post.objects.all().order_by("-date")[:3]
     return render(request, 'blog/index.html',{
         "post": latest_post,
     })
 
 def posts(request):
+    post_data = Post.objects.all()
     return render(request, 'blog/all-posts.html', {
         "all_posts": post_data,
     })
 
 def posts_details(request, slug):
-    identifier = next(items for items in post_data if items['slug'] == slug)
+    """
+    # sudah tidak perlu
+    identifier = next(items for items in post_data if items['slug'] == slug)  # pointer untuk memilih post berdasarkan slug
+    """
+    identifier = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/posts-details.html', {
         "post": identifier,
     })
