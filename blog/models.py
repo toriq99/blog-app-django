e902f33d93fe4d5bffa403c4f5c84dd567795a91
Thariq <thariq879@gmail.com>
2023-02-14 10:32:35 +0700

    adding comment in SinglePostView(DetailView)

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150, null=True)
     excerpt = models.CharField(max_length=50, null=True)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
-    image_name = models.CharField(max_length=50, null=True)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True, null=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)], null=True)
@@ -30,3 +30,9 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title}, {self.author}"
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField(max_length=254)
+    text = models.TextField(max_length=500)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from datetime import date
+from django.views.generic import ListView, DetailView
 
+from datetime import date
 from .models import *
+from .forms import CommentForm
 
 # Create your views here.
 
-def index(request):
-    latest_post = Post.objects.all().order_by("-date")[:3]
-    return render(request, 'blog/index.html',{
-        "post": latest_post,
-    })
+class IndexView(ListView):              # Rubah index view kedalam ListView
+    model = Post
+    template_name = "blog/index.html"
+    ordering = ["-date"]                # menentukan order by
+    context_object_name = "post"        # merubah object data di view menjadi posts
 
-def posts(request):
-    post_data = Post.objects.all()
-    return render(request, 'blog/all-posts.html', {
-        "all_posts": post_data,
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
+class AllPostView(ListView):
+    model = Post
+    template_name = "blog/all-posts.html"
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+# post detail using class based
+class SinglePostView(DetailView):
+    model = Post
+    template_name = "blog/posts-details.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tag"] = self.object.tags.all()
+        context["comment_form"] = CommentForm()
+        return context
+    
+
+"""
 def posts_details(request, slug):
-    """
+  
     # sudah tidak perlu
-    identifier = next(items for items in post_data if items['slug'] == slug)  # pointer untuk memilih post berdasarkan slug
-    """
+    # identifier = next(items for items in post_data if items['slug'] == slug)  # pointer untuk memilih post berdasarkan slug
+
     identifier = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/posts-details.html', {
         "post": identifier,
     })
+
+"""
